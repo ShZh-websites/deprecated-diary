2864a603041cfc039de712aabf2c4fd0a836d951
imShZh <zhihaoshen7@gmail.com>
2021-12-02 23:40:08 +0800

    feat(api): use api rather than hard code.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,34 @@
-<script>
-  import News from "$lib/News.svelte"
+<script context="module">
+  export async function load({ fetch }) {
+    const news =  await fetch('/api/news.json').then((r) => r.json());
+    return {
+      props: { news }
+    }
+  }
 </script>
 
+<script>
+  import News from "$lib/News.svelte"
+
+  export let news
+</script>
+
+<svelte:head>
+  <title>日记本 ｜ 首页</title>
+</svelte:head>
+
 <figure>
-  <figcaption class="title">ShZh的小站</figcaption>
+  <figcaption class="title">ShZh的日记本</figcaption>
   <img src="https://res.cloudinary.com/shzh7/image/upload/v1638439744/szh_pg8oum.png" alt="szh" class="avatar">
 </figure>
 <section class="intro">
-  <p>Hi，我是沈之豪，这是我的个人小站，也是我写自己生活经历和各种感悟的地方。</p>
+  <p>Hi，我是沈之豪，这是我的日记小站，也是我写自己生活经历和各种感悟的地方。</p>
   <p>和我的技术博客不同，博客里面全都是冷冰冰的技术博文，而这里的内容更有人情，更有烟火味。</p>
   <p>当然了，毕竟我本身是计算机科学与技术专业，所以里面的内容免不了要和技术沾点边。</p>
   <p>希望我的文字能给你带来一点启发，如果你觉得对你有帮助，你可以给我买一杯咖啡☕️。</p>
 </section>
 
-<News/>
+<News {news}/>
 
 <style lang="scss">
   figure {
