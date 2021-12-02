d037c5747350f775267a8e076fe022fe271f0e1a
imShZh <zhihaoshen7@gmail.com>
2021-12-02 15:12:15 +0800

    feat(index): finish basic layout of index page.

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,3 +1,16 @@
 <nav>
-    This is navigation bar
+    <ul>
+        <li>
+            <a href="">首页</a>
+        </li>
+        <li>
+            <a href="">归档</a>
+        </li>
+        <li>
+            <a href="">留言</a>
+        </li>
+        <li>
+            <a href="">关于</a>
+        </li>
+    </ul>
 </nav>
