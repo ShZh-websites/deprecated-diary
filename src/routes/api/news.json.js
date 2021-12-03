cd05e30d16a3a1ceb892be0646c8a5f9b38f8cee
imShZh <zhihaoshen7@gmail.com>
2021-12-03 10:20:30 +0800

    feat(api): add svelte kit api.

---
+++ src/routes/api/news.json.js
@@ -0,0 +1,19 @@
+export function get() {
+  const news = [
+    {
+      date: 'Dec 1, 2021',
+      content: '✏️ 毕业设计开始选题，今天开始找老师'
+    },
+    {
+      date: 'Nov 12, 2021',
+      content: '🏠 回家修生养息，陪陪亲人',
+    },
+    {
+      date: 'Sept 29, 2021',
+      content: '🏫 在武大的最后一学期，且行且珍惜。。。',
+    },
+  ]
+  const body = JSON.stringify(news);
+
+  return {body}
+}
