2576de5ca2284387773ab3593b03af5ea62a98d4
imShZh <zhihaoshen7@gmail.com>
2021-12-03 23:27:00 +0800

    fix(api): change from getDay() to getDate().

--- src/utils/date.js
+++ src/utils/date.js
@@ -15,13 +15,13 @@ export function dateFormatEN(date) {
     case 12: monthAbbr = 'Dec'; break;
   }
 
-  return `${monthAbbr} ${date.getDay()}`;
+  return `${monthAbbr} ${date.getDate()}`;
 }
 
 export function dateFormatCN(date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  const day = date.getDay();
+  const day = date.getDate();
 
   return `${year}年${month}月${day}日`;
 }
