e33f9b304153b59e2e010f7c2bc4c7658241a1b0
imShZh <zhihaoshen7@gmail.com>
2022-01-16 16:20:54 +0800

    feat(about): add tech cards to about page.

---
+++ src/lib/TechCard.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let imageUrl;
+  export let techName;
+  export let description;
+</script>
+
+<figure>
+  <img src={imageUrl} alt={techName} />
+  <hr />
+  <figcaption>
+    <h2>{techName}</h2>
+    <p>{description}</p>
+  </figcaption>
+</figure>
+
+<style lang="scss">
+  figure {
+    text-align: center;
+    width: 200px;
+    margin: 0 20px;
+    padding-bottom: 0.5rem;
+    border-radius: 0.25rem;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+    img {
+      width: 200px;
+      height: 175px;
+    }
+
+    hr {
+      height: 0;
+      border-top-width: 1px;
+      border-style: solid;
+      border-color: rgb(229, 231, 235);
+    }
+
+    figcaption {
+      padding: 0 1rem;
+
+      h2 {
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+      }
+
+      p {
+        margin-top: 0;
+      }
+    }
+  }
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,23 +1,65 @@
-<div class="container panel">
-  <h1>自愿付费</h1>
-  <p>喜欢我的文章的人，可以自愿付费。你可以使用以下方式付款。</p>
-  <div class="payment">
-    <img
-      src="https://res.cloudinary.com/shzh7/image/upload/v1637030166/payment/alipay_x0lesm.jpg"
-      alt="支付宝付款"
-      class="payment-img"
-    />
-    <img
-      src="https://res.cloudinary.com/shzh7/image/upload/v1637030172/payment/wechat_pay_pm5ktx.png"
-      alt="微信付款"
-      class="payment-img"
-    />
+<script>
+  import TechCard from "$lib/TechCard.svelte";
+
+  let techniques = [
+    {
+      techName: "Svelte",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+      description: "🛠️ Cybernetically enhanced web apps."
+    },{
+      techName: "SvelteKit",
+      imageUrl: "https://kit.svelte.dev/_app/assets/svelte-kit-machine-f9e1f4d5.avif",
+      description: "📦 The fastest way to build Svelte apps."
+    }, {
+      techName: "Vite",
+      imageUrl: "https://vitejs.dev/logo.svg",
+      description: "⚡️ Next Generation Frontend Tooling."
+    },
+  ]
+</script>
+
+<div class="container">
+  <div class="panel">
+    <h1>技术选型</h1>
+    <p>本站点使用了下列技术框架，非常感谢它们带来的便利。</p>
+    <div class="cards">
+      {#each techniques as technique}
+        <TechCard
+          techName={technique.techName}
+          imageUrl={technique.imageUrl}
+          description={technique.description}
+        />
+      {/each}
+    </div>
+  </div>
+  <div class="panel">
+    <h1>自愿付费</h1>
+    <p>喜欢我的文章的人，可以自愿付费。你可以使用以下方式付款。</p>
+    <div class="payment">
+      <img
+        src="https://res.cloudinary.com/shzh7/image/upload/v1637030166/payment/alipay_x0lesm.jpg"
+        alt="支付宝付款"
+        class="payment-img"
+      />
+      <img
+        src="https://res.cloudinary.com/shzh7/image/upload/v1637030172/payment/wechat_pay_pm5ktx.png"
+        alt="微信付款"
+        class="payment-img"
+      />
+    </div>
   </div>
 </div>
 
 <style lang="scss">
   .container {
     text-align: center;
+
+    .cards {
+      display: flex;
+      justify-content: center;
+      margin-top: 2rem;
+      margin-bottom: 2rem;
+    }
 
     .payment {
       margin-top: 2rem;
@@ -29,8 +71,10 @@
       align-items: center;
 
       .payment-img {
-        height: 24rem;
+        height: 22rem;
         margin: 0 2.5rem;
+        border-radius: 0.25rem;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
       }
     }
   }
